4a6740ab9e8825ff1bf29eb298001c39ac71b8cc
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2025-10-19 10:44:28 +0200

    Add MDX support with new dependencies and components

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 	images: {
 		remotePatterns: [new URL("https://placehold.co/**")],
 	},
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
