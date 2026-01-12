c335473c7de40b553b12481456fa3a2069f0ec11
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-01-12 15:34:03 +0100

    Update logo dimensions and adjust CSS variables for improved styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Footer() {
 	return (
-		<footer className="w-full py-4 border-t border-primary/20">
-			<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-50 h-auto" />
+		<footer className="w-full p-4 border-t border-foreground/20">
+			<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-30 h-auto" />
 		</footer>
 	);
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Header() {
 	return (
 		<div className="pb-16">
-			<header className="fixed top-0 left-0 z-10 w-full h-16 px-8 flex flex-row items-center justify-between border-b border-primary/20 bg-background/50 backdrop-blur-md">
-				<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-50 h-auto" />
+			<header className="fixed top-0 left-0 z-10 w-full h-16 px-8 flex flex-row items-center justify-between border-b border-foreground/20 bg-background/50 backdrop-blur-md">
+				<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-30 h-auto" />
 				<ThemeToggle />
 			</header>
 		</div>
