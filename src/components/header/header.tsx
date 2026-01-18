78b8dc22ae164d4bb0a798964605dab9ebdcffdd
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-01-18 22:25:52 +0100

    Refactor header component for improved navigation structure and styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,77 +20,48 @@ export default function Header() {
 	return (
 		<div className="pb-16">
 			<header className="fixed top-0 left-0 z-10 w-full h-16 px-8 flex flex-row items-center justify-between border-b border-foreground/20 bg-background/50 backdrop-blur-md">
-				<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-auto h-12" />
-				<NavigationMenu viewport={isMobile}>
-					<NavigationMenuList className="flex-wrap">
-						<NavigationMenuItem>
-							<NavigationMenuTrigger>Home</NavigationMenuTrigger>
-							<NavigationMenuContent>
-								<div className="flex flex-row">
-									<NavigationMenuLink asChild>
-										<Link href="/lessons" className="w-64 p-8 flex flex-col justify-end">
-											<h2>Lessons</h2>
-											<span className="text-muted-foreground">Learn at your own pace and track your progress.</span>
-										</Link>
-									</NavigationMenuLink>
-									<div>
+				<div className="flex flex-row items-center h-full gap-8">
+					<Link href="/">
+						<Image alt="Logo" src="/logo_full.svg" width={0} height={0} className="dark:invert w-auto h-12" />
+					</Link>
+					<NavigationMenu viewport={isMobile}>
+						<NavigationMenuList className="flex-wrap">
+							<NavigationMenuItem>
+								<NavigationMenuTrigger className="bg-transparent">Lessons</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<div className="flex flex-row">
 										<NavigationMenuLink asChild>
-											<Link href="/lessons?beginner" className="p-3 w-48">
-												<span className="font-bold leading-none">Beginner</span>
-												<span className="text-muted-foreground leading-none">Start your journey</span>
+											<Link href="/lessons" className="w-64 p-8 flex flex-col justify-end">
+												<h2>Lessons</h2>
+												<span className="text-muted-foreground">Learn at your own pace and track your progress.</span>
 											</Link>
 										</NavigationMenuLink>
-										<NavigationMenuLink asChild>
-											<Link href="/lessons?intermediate" className="p-3 w-48">
-												<span className="font-bold leading-none">Intermediate</span>
-												<span className="text-muted-foreground leading-none">Level up your skills</span>
-											</Link>
-										</NavigationMenuLink>
-										<NavigationMenuLink asChild>
-											<Link href="/lessons?advanced" className="p-3 w-48">
-												<span className="font-bold leading-none">Advanced</span>
-												<span className="text-muted-foreground leading-none">Master the concepts</span>
-											</Link>
-										</NavigationMenuLink>
+										<div>
+											<NavigationMenuLink asChild>
+												<Link href="/lessons?beginner" className="p-3 w-48">
+													<span className="font-bold leading-none">Beginner</span>
+													<span className="text-muted-foreground leading-none">Start your journey</span>
+												</Link>
+											</NavigationMenuLink>
+											<NavigationMenuLink asChild>
+												<Link href="/lessons?intermediate" className="p-3 w-48">
+													<span className="font-bold leading-none">Intermediate</span>
+													<span className="text-muted-foreground leading-none">Level up your skills</span>
+												</Link>
+											</NavigationMenuLink>
+											<NavigationMenuLink asChild>
+												<Link href="/lessons?advanced" className="p-3 w-48">
+													<span className="font-bold leading-none">Advanced</span>
+													<span className="text-muted-foreground leading-none">Master the concepts</span>
+												</Link>
+											</NavigationMenuLink>
+										</div>
 									</div>
-								</div>
-							</NavigationMenuContent>
-						</NavigationMenuItem>
-
-						<NavigationMenuItem>
-							<NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-								<Link href="/docs">Docs</Link>
-							</NavigationMenuLink>
-						</NavigationMenuItem>
-						<NavigationMenuItem className="hidden md:block">
-							<NavigationMenuTrigger>List</NavigationMenuTrigger>
-							<NavigationMenuContent>
-								<ul className="grid w-[300px] gap-4">
-									<li>
-										<NavigationMenuLink asChild>
-											<Link href="#">
-												<div className="font-medium">Components</div>
-												<div className="text-muted-foreground">Browse all components in the library.</div>
-											</Link>
-										</NavigationMenuLink>
-										<NavigationMenuLink asChild>
-											<Link href="#">
-												<div className="font-medium">Documentation</div>
-												<div className="text-muted-foreground">Learn how to use the library.</div>
-											</Link>
-										</NavigationMenuLink>
-										<NavigationMenuLink asChild>
-											<Link href="#">
-												<div className="font-medium">Blog</div>
-												<div className="text-muted-foreground">Read our latest blog posts.</div>
-											</Link>
-										</NavigationMenuLink>
-									</li>
-								</ul>
-							</NavigationMenuContent>
-						</NavigationMenuItem>
-					</NavigationMenuList>
-				</NavigationMenu>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+				</div>
 				<ThemeToggle />
 			</header>
 		</div>
