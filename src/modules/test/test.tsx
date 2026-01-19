b78c58e5783e6088767363b8931a9dffc592b09f
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-01-19 01:54:38 +0100

    Add hand recognition module content

--- src/modules/test/test.tsx
+++ src/modules/test/test.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import QRCode from "react-qr-code";
-
 export default function Test() {
-	return <QRCode value="https://example.com" />;
+	return <span>AAAA</span>;
 }
